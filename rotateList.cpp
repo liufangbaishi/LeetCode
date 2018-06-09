262c78f1799266af8eff073a2a358e0f8ab32a14
liufangbaishi <1728672994@qq.com>
2018-06-09 22:51:50 +0800

    右旋链表

---
+++ rotateList.cpp
@@ -0,0 +1,61 @@
+// rotate list  1->2->3->4->5->N && k==2   4->5->1->2->3->N
+struct ListNode 
+{
+	int val;     
+	ListNode *next;
+    ListNode(int x) 
+		: val(x), next(NULL) 
+	{}
+};
+ListNode *rotateRight(ListNode *head, int k) 
+{
+	if (head == NULL || head->next == NULL)
+		return head;
+	ListNode* p = head;
+	int count = 0;
+	while (p)
+	{
+		count++;
+		p = p->next;
+	}
+	k %= count;
+	if (k == 0)
+		return head;
+	ListNode* cur = head;
+	ListNode* pre = head;
+	int num = count - k;
+	while (num--)
+	{
+		pre = cur;
+		cur = cur->next;
+	}
+	pre->next = NULL;
+	ListNode* pHead = cur;
+	while (cur->next)
+		cur = cur->next;
+	cur->next = head;
+	return pHead;
+}
+
+// 测试
+void PushFront(ListNode* &head, int x)
+{
+	if (head == NULL)
+		head = new ListNode(x);
+	else
+	{
+		ListNode* tmp = new ListNode(x);
+		tmp->next = head;
+		head = tmp;
+	}
+}
+void test()
+{
+	ListNode* p = NULL;
+	PushFront(p, 5);
+	PushFront(p, 4);
+	PushFront(p, 3);
+	PushFront(p, 2);
+	PushFront(p, 1);
+	ListNode* newhead = rotateRight(p, 5);
+}
